2540d5a5ff3fd61b7b6a11f1683aaa711c5ad068
naeembaloch9099 <nbcuilahore@gmail.com>
2025-08-20 14:41:02 +0500

    i make the profile card by using props

--- src/index.js
+++ src/index.js
@@ -6,12 +6,18 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 function App() {
   return (
     <>
-      <h1>rendring</h1>
+      <h1>Profile Card</h1>
       <Pizza
         name="Muhammad Naeem"
-        regno="1223"
-        picture="/logo192.png"
+        Description="Muhammad naeem computer enigneer form comsats university islamabad lahore campus"
+        picture="/img.jpg"
         alt="logo"
+        info1="html css"
+        info2="javscript"
+        info3="git and github"
+        info4="html css"
+        info5="javscript"
+        info6="git and github"
       />
     </>
   );
@@ -19,9 +25,18 @@ function App() {
 function Pizza(props) {
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Reg No: {props.regno}</p>
       <img src={props.picture} alt="Logo" />
+      <h2>{props.name}</h2>
+      <p>{props.Description}</p>
+      <h2>Skills</h2>
+      <div className="button-container">
+        <button>{props.info1}</button>
+        <button>{props.info2}</button>
+        <button>{props.info3}</button>
+        <button>{props.info4}</button>
+        <button>{props.info5}</button>
+        <button>{props.info6}</button>
+      </div>
     </div>
   );
 }
